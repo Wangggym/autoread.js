f1c73e84f289c3e7d8b52f71a8f211f390924b99
王毅敏 <wangyimin@jusda.int>
2019-11-29 17:28:41 +0800

    增加检测页面阅读时间

--- autoReadScript.js
+++ autoReadScript.js
@@ -1,26 +1,37 @@
-var nobrs = document.getElementsByTagName('nobr');
+var nobrs = document.getElementById('w_code').contentWindow.document.getElementsByTagName('nobr');
 console.log(nobrs);
 function isCompleted(element) {
     var completed = element.getElementsByClassName('completed');
     return !!(completed && completed.length)
 }
-function executeReadClass(nobr, order, maxExcuteTime) {
+function executeReadClass(nobr, order) {
     nobr.getElementsByTagName('a')[0].click();
-    let count = 0;
-    var interval = null;
     return new Promise((resolve, reject) => {
-        interval = setInterval(() => {
-            count++;
-            if (isCompleted(nobr)) {
-                clearInterval(interval);
-                return resolve("执行成功：" + order);
-            } else if (count === maxExcuteTime / 2) {
-                clearInterval(interval);
-                return resolve("执行失败" + order);
-            }
-            (function () { console.log("==执行阅读任务== 当前任务No:" + order); })()
-        }, 2000);
+        setTimeout(() => {
+            let count = 0;
+            var interval = null;
+            var readTime = getReadTime();
+            interval = setInterval(() => {
+                count++;
+                if (isCompleted(nobr)) {
+                    clearInterval(interval);
+                    return resolve(`执行成功:${order},执行时间:${count},阅读时间:${readTime}`);
+                } else if (count === readTime) {
+                    clearInterval(interval);
+                    return resolve(`执行失败:${order},执行时间:${count},阅读时间:${readTime}`);
+                }
+                (function () { console.log("==执行阅读任务== 当前任务No:" + order); })()
+            }, 1000);
+        }, 5000);
     });
+}
+function getReadTime() {
+    var readTimeString = document.getElementById('w_lms_content').contentWindow.document.getElementsByTagName('table')[1].getElementsByTagName('td')[0].innerText
+    var patt1 = /习[0-9]+秒/;
+    var patt2 = /[0-9]+/;
+    var readTime = Number(readTimeString.match(patt1)[0].match(patt2)[0]) + 60;
+    console.log(`预计需要阅读时间:${readTime}`);
+    return readTime;
 }
 async function excuteRead() {
     for (var i = 0; i < nobrs.length; i++) {
@@ -28,7 +39,7 @@ async function excuteRead() {
         var notattempts = nobr.getElementsByClassName('notattempt');
         var incompletes = nobr.getElementsByClassName('incomplete');
         if ((notattempts && notattempts.length) || (incompletes && incompletes.length)) {
-            var result = await executeReadClass(nobr, i, 120)
+            var result = await executeReadClass(nobr, i)
             console.log(result)
         }
     }
@@ -37,7 +48,7 @@ async function excuteRead() {
         var notattempts = nobr.getElementsByClassName('notattempt');
         var incompletes = nobr.getElementsByClassName('incomplete');
         if ((notattempts && notattempts.length) || (incompletes && incompletes.length)) {
-            var result = await executeReadClass(nobr, i, 180)
+            var result = await executeReadClass(nobr, i)
             console.log(result)
         }
     }
